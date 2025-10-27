026c006357f25581b66011cef6b61bc5f5a1968f
carissaura <carissaaura0505@gmail.com>
2025-10-27 20:53:38 +0700

    Buat File 1 index.php
    File 1 myphp-app

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+  echo "<h1>Hello from PHP App!</h1>";
+  echo "<p><h2> Name: Carissa Aura Bethari </h2></p>";
+  echo "<p><h2> NIM: 2210512046 </h2></p>";
+  echo "<p><h2> Kelas A Cloud Computing - S1 Sistem Informasi </h2></p>";
+  echo "<p>Deployed via DevOps CI/CD pipeline</p>";
+?>
